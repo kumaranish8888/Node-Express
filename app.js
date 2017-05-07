95b3c1f1e3007da01f46090fa98aad114cb5fb11
kumaranish8888 <kumaranish8888@gmail.com>
2017-05-07 13:26:10 -0400

    Added code

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var path = require('path');
 var expressValidator = require('express-validator');
+var mongojs = require('mongojs');
+var db = mongojs(customerapp, ['users']);
 
 var app = express();
 /*
@@ -29,6 +31,13 @@ app.use(bodyParser.urlencoded({extended: false}));
 //Set static path, This path file will overwrite any data in app.js file while rendering
 app.use(express.static(path.join(__dirname, 'public')));
 
+//Global vars
+app.use(function(req, res, next){
+    res.locals.errors = null;
+    next();
+})
+
+//Express validator Middleware
 app.use(expressValidator({
   errorFormatter: function(param, msg, value) {
       var namespace = param.split('.')
@@ -77,6 +86,9 @@ var users = [
 
 
 app.get('/', function(req, res){
+    db.users.find(function (err, docs) {
+	// docs is an array of all the documents in mycollection
+    })
     res.render('index', {
         title: "Customers",
         users: users
@@ -84,12 +96,28 @@ app.get('/', function(req, res){
 }) 
 
 app.post('/users/add', function(req, res){
-    var newUser = {
-        first_name : req.body.first_name,
-        last_name : req.body.last_name,
-        email : req.body.email
+    
+    req.checkBody('first_name', 'First Name is Requied').notEmpty();
+    req.checkBody('last_name', 'Last Name is Requied').notEmpty();
+    req.checkBody('email', 'Email is Requied').notEmpty();
+    
+    var errors = req.validationErrors();
+    
+    if(errors){
+        res.render('index', {
+            title: "Customers",
+            users: users,
+            errors: errors
+        });
+    }else{
+        var newUser = {
+            first_name : req.body.first_name,
+            last_name : req.body.last_name,
+            email : req.body.email
+        }
+        console.log('SUCCESS');
     }
-    console.log(newUser);
+    
 }) 
 
 
